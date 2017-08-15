b7630f368cb4923aacf822ce38f1310a1bcd11cf
marcorisi <marco@supermercato24.it>
2017-08-15 19:26:24 +0200

    Implementing next mutators that calculates next generation cells

--- src/store.js
+++ src/store.js
@@ -30,6 +30,92 @@ var store = new Vuex.Store({
       state.COLS = _COLS;
 
       console.log('game initialized');
-    }
-  }
+    },
+
+    // calculate the next iteration of the Conway Game of Life
+    next (state) {
+
+      // Returns the matrix index of the neighbour cells
+      // identified by x and y coordinates
+      var getNeighbourCoordinates = function (x, y, ROWS, COLS) {
+        var xMin = Math.max(0, x-1),
+            xMax = Math.min(ROWS-1, x+1),
+            yMin = Math.max(0, y-1),
+            yMax = Math.min(COLS-1, y+1),
+            coordinates = [];
+
+        for (var _x = xMin; _x <= xMax; _x++) {
+          for (var _y = yMin; _y <= yMax; _y++) {
+            // exclude target cell
+            if ((_x != x) || (_y != y)) {
+              coordinates.push({
+                x: _x,
+                y: _y,
+              });
+            }
+          }
+        }
+
+        return coordinates;
+      };
+
+      // Determines the next status for the given cell
+      // according to the game of life rules.
+      var next = function (x, y, neighbourCoordinates) {
+
+        // lists possible outcome of each iteration
+        var UNDERPOPULATION = false,
+            SURVIVE = true,
+            OVERPOPULATION = false,
+            REPRODUCTION = true;
+
+        var amILive = state.matrix[x][y],
+            livingCells = 0,
+            next = amILive; // by default, return current state
+
+        // calculates the number of living cells around the given one.
+        for (var ii = 0, c = neighbourCoordinates.length; ii < c; ii++) {
+          var coords = neighbourCoordinates[ii];
+          livingCells += state.matrix[coords.x][coords.y] ? 1 : 0;
+        }
+
+        // 1. Any live cell with fewer than two live neighbours dies,
+        // as if caused by underpopulation
+        if (amILive && livingCells < 2) {
+          next = UNDERPOPULATION;
+        }
+        // 2. Any live cell with two or three live neighbours
+        // lives on to the next generation.
+        else if (amILive && (livingCells === 2 || livingCells === 3)) {
+          mext = SURVIVE;
+        }
+        // 3. Any live cell with more than three live neighbours dies,
+        // as if by overpopulation.
+        else if (amILive && livingCells > 3) {
+          next = OVERPOPULATION;
+        }
+        // 4. Any dead cell with exactly three live neighbours becomes a live cell,
+        // as if by reproduction
+        else if (!amILive && livingCells === 3) {
+          next = REPRODUCTION;
+        }
+
+        return next;
+      };
+
+      // initialize a new matrix
+      var _matrix = new Array(state.ROWS);
+      for (var ii = 0; ii < state.ROWS; ii++) {
+        _matrix[ii] = new Array(state.COLS)
+        for (var jj = 0; jj < state.COLS; jj++) {
+          var neighbourCoordinates = getNeighbourCoordinates(ii, jj,
+            state.ROWS, state.COLS);
+          _matrix[ii][jj] = next(ii, jj, neighbourCoordinates);
+        }
+      }
+
+      // update matrix
+      state.matrix = _matrix;
+    },
+  },
 });
